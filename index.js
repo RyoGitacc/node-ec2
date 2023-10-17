079ea50aeed639bc69a79616ff9314e0466b1b7d
Ryo Aoyama <aoyama.in.opu@gmail.com>
2023-10-17 03:50:29 -0400

    port change

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(22, () => {
+app.listen(process.env.PORT || 3300, () => {
   console.log('Server started on port 3000');
 });
